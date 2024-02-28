ffffa1b6beecdc8194c9ec5cd1c6daa22a1f2c73
Nahom Abera <Nahomtesfahun001@gmail.com>
2024-02-27 20:45:25 -0500

    Add Surviellance System--2

---
+++ System_1_HowToOperateSurveillanceDrone.md
@@ -0,0 +1,36 @@
+## How to Operate Surveillance Drone 
+
+1. Turn on the drone
+2. Connect your computer wifi with the drone
+3. Run the System_1_SurveillanceDrone.py file either using an IDE or using your terminal/command line
+4. You'll have to windows on you screen, 
+   1. The pygame dark screen - use this screen to operate the drone by pressing the dark screen and then pressing the commands below.
+   2. The OpenCV screen - this screen will provide a live stream from the drone's camera
+
+
+# KeyBoard Commands
+Press:
+
+    e- to take off
+    
+    q- to land
+    
+    Left Arrow key- to move to the left
+
+    Right Arrow key- to move to the right
+
+    Upward Arrow key- to move forward
+
+    Down Arrow key- to move backward
+
+    w- to move up
+
+    s- to move down
+    
+    a- to rotate clockwise direction
+
+    d- to rotate counter clockwise direction
+
+    r- to take snapshot
+    
+    
---
+++ System_1_SurveillanceDrone.py
@@ -0,0 +1,61 @@
+from djitellopy import tello
+import time
+import cv2
+import KeyBoardCommand as kbc
+
+
+kbc.init()
+drn = tello.Tello()
+drn.connect()
+print(drn.get_battery())
+
+global strm
+drn.streamon()
+def KeyBoardInput():
+    LeftRight = 0
+    ForwardBackward = 0
+    UpDown = 0
+    YV = 0
+    speed = 50
+
+    if kbc.getKey("q"):
+        drn.land()
+        time.sleep(3)
+
+    if kbc.getKey("e"):
+        drn.takeoff()
+
+    if kbc.getKey("LEFT"):
+        LeftRight = -speed
+    elif kbc.getKey("RIGHT"):
+        LeftRight = speed
+
+    if kbc.getKey("UP"):
+        ForwardBackward = speed
+    elif kbc.getKey("DOWN"):
+        ForwardBackward = -speed
+
+    if kbc.getKey("w"):
+        UpDown = speed
+    elif kbc.getKey("s"):
+        UpDown = -speed
+
+    if kbc.getKey("a"):
+        YV = speed
+    elif kbc.getKey("d"):
+        YV = -speed
+
+    if kbc.getKey("r"):
+        cv2.imwrite(f'Images/{time.time()}.jpg', strm)
+        time.sleep(0.3)
+
+    return [LeftRight, ForwardBackward, UpDown, YV]
+
+
+while True:
+    val = KeyBoardInput()
+    drn.send_rc_control(val[0], val[1], val[2], val[3])
+    strm = drn.get_frame_read().frame
+    strm = cv2.resize(strm, (480, 480))
+    cv2.imshow("Image", strm)
+    cv2.waitKey(1)
